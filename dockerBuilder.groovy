952cff3cda6759f4bf4dbcdd1a54179374ea27a4
santosh3702 <santoshrahul81@gmail.com>
2019-02-07 18:48:38 +0530

    Create dockerBuilder.groovy

---
+++ dockerBuilder.groovy
@@ -0,0 +1,19 @@
+import org.jenkinsci.plugins.dockerbuildstep.*
+import net.sf.json.JSONObject
+import org.kohsuke.stapler.StaplerRequest
+
+def instance = Jenkins.getInstance()
+  
+def docker = instance.getDescriptor(DockerBuilder.class)
+
+JSONObject myString = new JSONObject()
+
+myString.put("dockerUrl", "unix:///var/run/docker.sock")
+
+myString.put("dockerVersion", "")
+myString.put("dockerCertPath", "")
+
+
+docker.configure(null,myString)
+
+instance.save()
